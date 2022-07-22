a9e18847d3a3bf0ae33af6a6bbfa537cacd104e8
Nurio Fernandez <personal@nurio.me>
2022-07-22 02:10:05 +0200

    Added a simple JWT implementation

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Glaucus/Bracelet
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/brianvoe/sjwt v0.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- httpServer/controllers/login/password/controller.go
+++ httpServer/controllers/login/password/controller.go
@@ -1,6 +1,8 @@
 package password
 
 import (
+	"github.com/brianvoe/sjwt"
+
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -11,5 +13,14 @@ func Controller(w http.ResponseWriter, r *http.Request) {
 	var request Request
 	json.Unmarshal(reqBody, &request)
 
-	json.NewEncoder(w).Encode(Response{Token: "random-token"})
+	// Set Claims
+	claims := sjwt.New()
+	claims.Set("username", request.Username)
+	claims.Set("user_id", -1)
+
+	// Generate jwt
+	secretKey := []byte("random_secret_key_will_be_here")
+	jwt := claims.Generate(secretKey)
+
+	json.NewEncoder(w).Encode(Response{Token: jwt})
 }
